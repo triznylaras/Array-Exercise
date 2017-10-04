3b09eebdd8bf2d4271f2ab76445f361e952ad737
Trizny Larasati <triznylaras@gmail.com>
2017-10-04 13:14:06 +0700

    Commit Pertama

---
+++ src/ArrayOne.java
@@ -0,0 +1,9 @@
+
+public class ArrayOne {
+	public static void main(String[] args) {
+		String[] color = {"white", "green", "red", "blue", "black"};
+		
+		System.out.print("The memory of that scene for me is like a frame of film forever frozen at that moment: ");
+		System.out.print("\nthe " + color[2] + " carpet, the " + color[1] + " lawn, the " + color[0] + " house, the leaden sky. \nThe new president and his first lady. - Richard M.Nixon");
+	}
+}
